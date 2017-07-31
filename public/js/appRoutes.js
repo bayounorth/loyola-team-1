7f76862967aaf28194a1c074d8a8847e8c0c81c9
andresn93 <andresn93@gmail.com>
2017-07-31 14:31:08 -0500

    controller update

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -10,7 +10,7 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 
 		.when('/faq', {
 			templateUrl: 'views/faq.html',
-			controller: 'NerdController'
+			controller: 'FaqController'
 		})
 
 		.when('/applied', {
