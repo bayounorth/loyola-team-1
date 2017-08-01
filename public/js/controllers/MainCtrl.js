052c33133e72ec37a49b0a3574f3460b13af571e
Gabe <gabriel.philbois@gmail.com>
2017-08-01 12:39:53 -0500

    menu view selection working

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,4 +2,21 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	$scope.tagline = 'To the moon and back!';	
 
+
+
+	//View selection for the side menu
+	$scope.menuView = 1;
+	console.log($scope.menuView);
+
+
+	$scope.showSubscribe = function(){
+		$scope.menuView = 2;
+		console.log($scope.menuView);
+	}
+	$scope.showApply = function(){
+		$scope.menuView = 3;
+		console.log($scope.menuView);
+	}
+
+
 });
