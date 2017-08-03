4133befb85ddaec47e92cf0eb1264b9d8657db74
Vicky Tran <tranvickyt@gmail.com>
2017-08-03 15:49:13 -0500

    divs and parallax

--- public/js/controllers/parallaxCtrl.js
+++ public/js/controllers/parallaxCtrl.js
@@ -1,5 +1,8 @@
 angular.module('parallaxCtrl', []).controller('ParallaxController', function($scope) {
 
+
+	$scope.text1 = "The Loyola Center for Entrepreneurship and Community Development (CECD) has partnered with Tech Talent South and software development studio LookFar to develop a new certificate program in software development.  This innovative 10-week program is designed to bridge the gap between the core skills learned in a liberal arts education and the starting point for a career in technology and software development."
 	$scope.img1 = "/img/images/KEN_4269.jpg";
+	$scope.img2 = "/img/images/KEN_0214.jpg";
 
 });
