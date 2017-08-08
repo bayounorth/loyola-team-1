68ad7ab2986db7722a402a07612179f9686c3e6d
Vicky Tran <tranvickyt@gmail.com>
2017-08-07 20:03:22 -0500

    changed out  header logo

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -39,11 +39,18 @@ angular.module('MainCtrl', ['typer']).controller('MainController', function($sco
 	        console.log($scope.applicationAllow );
 	    });
 
+// PARALLAX MATERIALS 
+
 	$scope.text1 = "The Loyola Center for Entrepreneurship and Community Development (CECD) has partnered with Tech Talent South and software development studio LookFar to develop a new certificate program in software development.  This innovative 10-week program is designed to bridge the gap between the core skills learned in a liberal arts education and the starting point for a career in technology and software development."
 	$scope.text3 = "Imagine Yourself Leveraging the Power of Technology to Solve Problems"
 	$scope.img1 = "/img/images/KEN_4269.jpg";
 	$scope.img2 = "/img/images/KEN_0214.jpg";
 
+
+// LOGOS
+
+	$scope.codeLogo = "img/logos/code_logo.svg"
+	$scope.codeLogoWhite = "img/logos/code_logo_white2.svg"
 
 
 	//menu links
